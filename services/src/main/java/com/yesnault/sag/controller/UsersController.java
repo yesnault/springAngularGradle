faf0f77c4908a973d0f98396a1d85b3eaf82ed68
Yvonnick Esnault <yvonnick.esnault@gmail.com>
2013-09-29 21:46:59 +0200

    Add combineJS & release plugin

--- services/src/main/java/com/yesnault/sag/controller/UsersController.java
+++ services/src/main/java/com/yesnault/sag/controller/UsersController.java
@@ -17,31 +17,31 @@ public class UsersController {
     @Autowired
     UserRepository userRepository;
 
-    @RequestMapping(value = "/sites", method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value = "/users", method = RequestMethod.GET, produces = "application/json")
     public @ResponseBody List<User> list() {
         return new ArrayList<User>(userRepository.findAll());
     }
 
-    @RequestMapping(value = "/sites/{id}", method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value = "/users/{id}", method = RequestMethod.GET, produces = "application/json")
     public @ResponseBody
     User getById(@PathVariable long id) {
         return userRepository.findOne(id);
     }
 
-    @RequestMapping(value = "/sites/{id}", method = RequestMethod.POST)
+    @RequestMapping(value = "/users/{id}", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void create(@RequestBody User user) {
         long id = user.getId();
         userRepository.save(user);
     }
 
-    @RequestMapping(value = "/sites/{id}", method = RequestMethod.PUT)
+    @RequestMapping(value = "/users/{id}", method = RequestMethod.PUT)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void update(@RequestBody User user) {
         userRepository.save(user);
     }
 
-    @RequestMapping(value = "/sites/{id}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/users/{id}", method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable long id) {
         userRepository.delete(id);
--- services/src/main/webapp/js/app/app.js
+++ services/src/main/webapp/js/app/app.js
@@ -1,19 +1,18 @@
-/*global define, angular */
+/* app.js */
 
 'use strict';
+var protoApp = angular.module('protoApp', [ 'ngRoute', 'ngResource']);
 
-    var protoApp = angular.module('protoApp', [ 'ngRoute', 'ngResource']);
+protoApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/home', {  controller: HomeCtrl, templateUrl: 'partials/home/home.html' })
+        .when('/login', {  controller: LoginCtrl, templateUrl: 'partials/login/login.html' })
 
-    protoApp.config(function ($routeProvider) {
-        $routeProvider
-            .when('/home', {  controller: HomeCtrl, templateUrl: 'partials/home/home.html' })
-            .when('/login', {  controller: LoginCtrl, templateUrl: 'partials/login/login.html' })
+        .when('/users/', { controller: UsersCtrl, templateUrl: 'partials/users/list.html'})
+        .when('/users/new/', {controller: UserNewCtrl, templateUrl: 'partials/users/detail.html'})
+        .when('/users/:userId', {controller: UserEditCtrl, templateUrl: 'partials/users/detail.html'})
 
-            .when('/users/', { controller: UsersCtrl, templateUrl: 'partials/users/list.html'})
-            .when('/users/new/', {controller: UserNewCtrl, templateUrl: 'partials/users/detail.html'})
-            .when('/users/:userId', {controller: UserEditCtrl, templateUrl: 'partials/users/detail.html'})
-
-            .otherwise({
-                redirectTo: '/login'
-            });
-    });
+        .otherwise({
+            redirectTo: '/login'
+        });
+});
